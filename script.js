934de4646213f45a18f26316833d5b0b5717bd08
Milena Kurzątkowska <kurzatkowska.milena@gmail.com>
2023-01-08 15:08:52 +0100

    Rename variables refactor and temperature rounding fix

--- script.js
+++ script.js
@@ -1,84 +1,78 @@
+const API_GEO_URL = 'http://api.openweathermap.org/geo/1.0/direct';
+const API_GEO_RESULT_LIMIT = 1;
+const API_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'e66112d233ee3ef50099df635a7ae89f';
+const API_UNITS = 'metric';
+const VALIDATION_MESSAGE = 'Wpisz poprawną nazwę miasta!';
+
 const cityInput = document.querySelector('input');
 const checkButton = document.querySelector('button');
 const cityNameElement = document.querySelector('.city-name');
 const validationElement = document.querySelector('.validation');
-const photo = document.querySelector('.photo');
-const weather = document.querySelector('.weather');
-const temperature = document.querySelector('.temperature');
-const humidity = document.querySelector('.humidity');
+const photoElement = document.querySelector('.photo');
+const weatherInfoElement = document.querySelector('.weather');
+const temperatureInfoElement = document.querySelector('.temperature');
+const humidityInfoElement = document.querySelector('.humidity');
 
-const API_GEO = 'http://api.openweathermap.org/geo/1.0/direct?q=';
-const API_LIMIT = '&limit=1';
-const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?';
-const API_KEY = '&appid=e66112d233ee3ef50099df635a7ae89f';
-const API_UNITS = '&units=metric';
+let longitude = null;
+let latitude = null;
+let cityNameValue = null;
 
-let lon = null;
-let lat = null;
-let cityNameResult = null;
-
-const setValidationMessage = () => {
-	validationElement.textContent = 'Wpisz poprawną nazwę miasta!';
-}
-
-const getWeather1 = async () => {
-	let geoResult = await axios.get(URL_GEO);
-	let lon1 = result.data[0].lat;
-};
-
-const setCoordinates = () => {
-	const city = cityInput.value;
-	const URL_GEO = API_GEO + city + API_LIMIT + API_KEY;
-	return axios.get(URL_GEO).then(res => {
-		lat = 'lat=' + res.data[0].lat;
-		lon = '&lon=' + res.data[0].lon;
-		cityNameResult = res.data[0].name;
+const setCoordinatesAndCity = () => {
+	const urlGeoParametrized = `${API_GEO_URL}?q=${cityInput.value}&limit=${API_GEO_RESULT_LIMIT}&appid=${API_KEY}`;
+	return axios.get(urlGeoParametrized).then(response => {
+		latitude = response.data[0].lat;
+		longitude = response.data[0].lon;
+		cityNameValue = response.data[0].name;
 	});
 };
 
+const setValidationMessage = () => {
+	validationElement.textContent = VALIDATION_MESSAGE;
+};
+
 const getWeather = () => {
-	const URL = API_LINK + lat + lon + API_KEY + API_UNITS;
+	const urlWeatherParametrized = `${API_WEATHER_URL}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=${API_UNITS}`;
+	return axios.get(urlWeatherParametrized).then(response => {
+		const temperature = response.data.main.temp;
+		const humidity = response.data.main.humidity;
+		const weather = response.data.weather[0];
 
-	axios.get(URL).then(res => {
-		const temp = res.data.main.temp;
-		const hum = res.data.main.humidity;
-		const status = res.data.weather[0];
-
-		cityNameElement.textContent = cityNameResult;
-
-		weather.textContent = status.main;
-		temperature.textContent = Math.floor(temp) + '°C';
-		humidity.textContent = hum + '%';
+		cityNameElement.textContent = cityNameValue;
+		weatherInfoElement.textContent = weather.main;
+		temperatureInfoElement.textContent = temperature.toFixed(1) + '°C';
+		humidityInfoElement.textContent = humidity + '%';
 
 		validationElement.textContent = '';
 		cityInput.value = '';
 
-		if (status.id >= 200 && status.id <= 232) {
-			photo.setAttribute('src', './img/thunderstorm.png');
-		} else if (status.id >= 300 && status.id <= 321) {
-			photo.setAttribute('src', './img/drizzle.png');
-		} else if (status.id >= 500 && status.id <= 531) {
-			photo.setAttribute('src', './img/rain.png');
-		} else if (status.id >= 600 && status.id <= 622) {
-			photo.setAttribute('src', './img/ice.png');
-		} else if (status.id >= 700 && status.id <= 781) {
-			photo.setAttribute('src', './img/fog.png');
-		} else if (status.id === 800) {
-			photo.setAttribute('src', './img/sun.png');
-		} else if (status.id >= 801 && status.id <= 804) {
-			photo.setAttribute('src', './img/cloud.png');
+		if (weather.id >= 200 && weather.id <= 232) {
+			photoElement.setAttribute('src', './img/thunderstorm.png');
+		} else if (weather.id >= 300 && weather.id <= 321) {
+			photoElement.setAttribute('src', './img/drizzle.png');
+		} else if (weather.id >= 500 && weather.id <= 531) {
+			photoElement.setAttribute('src', './img/rain.png');
+		} else if (weather.id >= 600 && weather.id <= 622) {
+			photoElement.setAttribute('src', './img/ice.png');
+		} else if (weather.id >= 700 && weather.id <= 781) {
+			photoElement.setAttribute('src', './img/fog.png');
+		} else if (weather.id === 800) {
+			photoElement.setAttribute('src', './img/sun.png');
+		} else if (weather.id >= 801 && weather.id <= 804) {
+			photoElement.setAttribute('src', './img/cloud.png');
 		} else {
-			photo.setAttribute('src', './img/unknown.png');
+			photoElement.setAttribute('src', './img/unknown.png');
 		}
 	});
 };
 
 async function showWeather() {
 	try {
-		await setCoordinates();
+		await setCoordinatesAndCity();
 		await getWeather();
 	} catch (err) {
-		setValidationMessage()
+		console.log(err);
+		setValidationMessage();
 	}
 }
 
