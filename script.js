c628a2d9a0f48ee621a9378331f588c3fd85a334
Milena Kurzątkowska <kurzatkowska.milena@gmail.com>
2023-01-08 17:05:46 +0100

    Refactor functions

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ const cityInput = document.querySelector('input');
 const checkButton = document.querySelector('button');
 const cityNameElement = document.querySelector('.city-name');
 const validationElement = document.querySelector('.validation');
-const photoElement = document.querySelector('.photo');
-const weatherInfoElement = document.querySelector('.weather');
+const imageElement = document.querySelector('.photo');
+const weatherDescriptionInfoElement = document.querySelector('.description');
 const temperatureInfoElement = document.querySelector('.temperature');
 const humidityInfoElement = document.querySelector('.humidity');
 
 let longitude = null;
 let latitude = null;
 let cityNameValue = null;
+let weatherDescriptionId = null;
 
 const setCoordinatesAndCity = () => {
 	const urlGeoParametrized = `${API_GEO_URL}?q=${cityInput.value}&limit=${API_GEO_RESULT_LIMIT}&appid=${API_KEY}`;
@@ -27,60 +28,75 @@ const setCoordinatesAndCity = () => {
 	});
 };
 
-const setValidationMessage = () => {
-	validationElement.textContent = VALIDATION_MESSAGE;
-};
-
-const getWeather = () => {
+const setWeather = () => {
 	const urlWeatherParametrized = `${API_WEATHER_URL}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=${API_UNITS}`;
 	return axios.get(urlWeatherParametrized).then(response => {
 		const temperature = response.data.main.temp;
 		const humidity = response.data.main.humidity;
-		const weather = response.data.weather[0];
+		const weatherDescription = response.data.weather[0];
 
 		cityNameElement.textContent = cityNameValue;
-		weatherInfoElement.textContent = weather.main;
+		weatherDescriptionInfoElement.textContent = weatherDescription.main;
 		temperatureInfoElement.textContent = temperature.toFixed(1) + '°C';
 		humidityInfoElement.textContent = humidity + '%';
-
-		validationElement.textContent = '';
-		cityInput.value = '';
-
-		if (weather.id >= 200 && weather.id <= 232) {
-			photoElement.setAttribute('src', './img/thunderstorm.png');
-		} else if (weather.id >= 300 && weather.id <= 321) {
-			photoElement.setAttribute('src', './img/drizzle.png');
-		} else if (weather.id >= 500 && weather.id <= 531) {
-			photoElement.setAttribute('src', './img/rain.png');
-		} else if (weather.id >= 600 && weather.id <= 622) {
-			photoElement.setAttribute('src', './img/ice.png');
-		} else if (weather.id >= 700 && weather.id <= 781) {
-			photoElement.setAttribute('src', './img/fog.png');
-		} else if (weather.id === 800) {
-			photoElement.setAttribute('src', './img/sun.png');
-		} else if (weather.id >= 801 && weather.id <= 804) {
-			photoElement.setAttribute('src', './img/cloud.png');
-		} else {
-			photoElement.setAttribute('src', './img/unknown.png');
-		}
+		weatherDescriptionId = weatherDescription.id;
 	});
 };
+
+const setWeatherImage = weatherDescriptonId => {
+	let imageSrc = './img/unknown.png';
+	switch (true) {
+		case weatherDescriptonId >= 200 && weatherDescriptonId < 300:
+			imageSrc = './img/thunderstorm.png';
+			break;
+		case weatherDescriptonId >= 300 && weatherDescriptonId < 400:
+			imageSrc = './img/drizzle.png';
+			break;
+		case weatherDescriptonId >= 500 && weatherDescriptonId < 600:
+			imageSrc = './img/rain.png';
+			break;
+		case weatherDescriptonId >= 600 && weatherDescriptonId < 700:
+			imageSrc = './img/ice.png';
+			break;
+		case weatherDescriptonId >= 700 && weatherDescriptonId < 800:
+			imageSrc = './img/fog.png';
+			break;
+		case weatherDescriptonId === 800:
+			imageSrc = './img/sun.png';
+			break;
+		case weatherDescriptonId >= 801 && weatherDescriptonId < 809:
+			imageSrc = './img/cloud.png';
+			break;
+		default:
+			imageSrc = './img/unknown.png';
+	}
+	imageElement.setAttribute('src', imageSrc);
+};
+
+const clearValidationMessage = () => (validationElement.textContent = '');
+const clearCityInput = () => (cityInput.value = '');
+
+const setValidationMessage = () =>
+	(validationElement.textContent = VALIDATION_MESSAGE);
 
 async function showWeather() {
 	try {
 		await setCoordinatesAndCity();
-		await getWeather();
+		await setWeather();
+		setWeatherImage(weatherDescriptionId);
+		clearValidationMessage();
+		clearCityInput();
 	} catch (err) {
 		console.log(err);
 		setValidationMessage();
 	}
 }
 
-const enterKey = e => {
+const showWeatherForEnterKey = e => {
 	if (e.key === 'Enter') {
 		showWeather();
 	}
 };
 
 checkButton.addEventListener('click', showWeather);
-cityInput.addEventListener('keyup', enterKey);
+cityInput.addEventListener('keyup', showWeatherForEnterKey);
