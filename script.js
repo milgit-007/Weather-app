415623690db35961cce913fd67856b500834da77
Milena Kurzątkowska <kurzatkowska.milena@gmail.com>
2023-03-30 20:05:18 +0200

    Refactor async function and update CSS/HTML files

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const API_GEO_RESULT_LIMIT = 1;
 const API_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'e66112d233ee3ef50099df635a7ae89f';
 const API_UNITS = 'metric';
-const VALIDATION_MESSAGE = 'Wpisz poprawną nazwę miasta!';
+const VALIDATION_MESSAGE = 'Please enter a valid city name!';
 
 const cityInput = document.querySelector('input');
 const checkButton = document.querySelector('button');
@@ -19,29 +19,28 @@ let latitude = null;
 let cityNameValue = null;
 let weatherDescriptionId = null;
 
-const setCoordinatesAndCity = () => {
+async function setCoordinatesAndCity() {
 	const urlGeoParametrized = `${API_GEO_URL}?q=${cityInput.value}&limit=${API_GEO_RESULT_LIMIT}&appid=${API_KEY}`;
-	return axios.get(urlGeoParametrized).then(response => {
-		latitude = response.data[0].lat;
-		longitude = response.data[0].lon;
-		cityNameValue = response.data[0].name;
-	});
-};
+	const response = await fetch(urlGeoParametrized, { method: 'GET' });
+	const data = await response.json();
+	latitude = data[0].lat;
+	longitude = data[0].lon;
+	cityNameValue = data[0].name;
+}
 
-const setWeather = () => {
-	const urlWeatherParametrized = `${API_WEATHER_URL}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=${API_UNITS}`;
-	return axios.get(urlWeatherParametrized).then(response => {
-		const temperature = response.data.main.temp;
-		const humidity = response.data.main.humidity;
-		const weatherDescription = response.data.weather[0];
-
-		cityNameElement.textContent = cityNameValue;
-		weatherDescriptionInfoElement.textContent = weatherDescription.main;
-		temperatureInfoElement.textContent = temperature.toFixed(1) + '°C';
-		humidityInfoElement.textContent = humidity + '%';
-		weatherDescriptionId = weatherDescription.id;
-	});
-};
+async function setWeather() {
+	const urlWeatherParameterized = `${API_WEATHER_URL}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=${API_UNITS}`;
+	const response = await fetch(urlWeatherParameterized, { method: 'GET' });
+	const data = await response.json();
+	const temperature = data.main.temp;
+	const humidity = data.main.humidity;
+	const weatherDescription = data.weather[0];
+	cityNameElement.textContent = cityNameValue;
+	weatherDescriptionInfoElement.textContent = weatherDescription.main;
+	temperatureInfoElement.textContent = temperature.toFixed(1) + '°C';
+	humidityInfoElement.textContent = humidity + '%';
+	weatherDescriptionId = weatherDescription.id;
+}
 
 const setWeatherImage = weatherDescriptionId => {
 	let imageSrc;
@@ -86,8 +85,8 @@ async function showWeather() {
 		setWeatherImage(weatherDescriptionId);
 		clearValidationMessage();
 		clearCityInput();
-	} catch (err) {
-		console.log(err);
+	} catch (error) {
+		console.error(error.message);
 		setValidationMessage();
 	}
 }
